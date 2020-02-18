dbe26d2d5963145af04c328ef954909178dfb4e2
Logan Negley <logannegley@gmail.com>
2020-02-18 15:33:00 -0600

    setValue function added in ULS

--- src/components/hooks/useLocalStorage.js
+++ src/components/hooks/useLocalStorage.js
@@ -4,7 +4,12 @@ export const useLocalStorage = (key, initalValue) => {
 const [storedValue, setStoredValue] = useState(() => {
     const item = window.useLocalStorage.getItem(key);
     return item ? JSON.parse(item) : initalValue;
-})
+});
+
+const setValue = value =>{
+    setStoredValue(value);
+    window.localStorage.setItem(key,JSON.stringigy(value));
+};
 
 return [storedValue]
 };

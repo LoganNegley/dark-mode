e8fed9b4fff1a738b9290abaf30f2b0517393155
Logan Negley <logannegley@gmail.com>
2020-02-18 18:40:09 -0600

    fixing errors

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+// import useDarkMode from '../components/hooks/useDarkMode';
 
 const Navbar = () => {
   const [darkMode, setDarkMode] = useState(false);
--- src/components/hooks/useDarkMode.js
+++ src/components/hooks/useDarkMode.js
@@ -2,6 +2,6 @@ import {useState} from 'react';
 import useLocalStorage from '../hooks/useLocalStorage';
 
 
-export default const useDarkMode = (key, initalValue) => {
-const [someValue, setSomeValue] = useLocalStorage('myKey')
+export default useDarkMode = (key, initalValue) => {
+const [darkMode, setDarkMode] = useLocalStorage('storedValue')
 };

83ab1a427d3b934539a074b6c64089a1de0406f2
Logan Negley <logannegley@gmail.com>
2020-02-19 18:38:44 -0600

    useEffect in useDarkMode created

--- src/components/hooks/useDarkMode.js
+++ src/components/hooks/useDarkMode.js
@@ -6,8 +6,11 @@ export default useDarkMode = (key, initalValue) => {
 const [darkMode, setDarkMode] = useLocalStorage(key,initalValue)
 
  useEffect(() => {
-
-  }, []);
-
+if(darkMode === 'ture'){
+document.querySelector('body').classList.add('dark-mode')
+}else{
+document.querySelector('body').classList.remove('dark-mode')
+}
+  }, [darkMode]);
 
 };
--- src/components/hooks/useLocalStorage.js
+++ src/components/hooks/useLocalStorage.js
@@ -1,9 +1,9 @@
 import {useState} from 'react';
 
 export const useLocalStorage = (key, initalValue) => {
-const [storedValue, setStoredValue] = useState(() => {
+    const [storedValue, setStoredValue] = useState(() => {
     const item = window.useLocalStorage.getItem(key);
-    return item ? JSON.parse(item) : initalValue;
+        return item ? JSON.parse(item) : initalValue;
 });
 
 const setValue = value =>{
